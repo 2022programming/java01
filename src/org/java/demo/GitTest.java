24fcbddbc9860cd30a5cb3058f29f7937cf11cf8
fss <fanshengsheng1992@icloud.com>
2022-10-22 23:22:16 +0800

    Fourth Commit

--- src/org/java/demo/GitTest.java
+++ src/org/java/demo/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("Master Test!");
         System.out.println("Push Test!");
+        System.out.println("Push Test1!");
     }
 }
